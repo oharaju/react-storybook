713185bde30e0a383b2cb5488f1a6f95397f9aeb
Júlia Ohara <juliaohara.br@gmail.com>
2023-08-08 21:39:09 -0300

    feat: adjust storybook background style

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,7 +3,7 @@ import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming'
 
 const theme = create({
-  base: 'light', // this will inherit the base properties of Storybooks'light theme
+  base: 'light',
 
   // Base color
   colorPrimary: '#ffff00',
@@ -11,8 +11,8 @@ const theme = create({
 
   // UI
   appBg: '#ffffff',
-  appContentBg: '#dd5588',
-  appBorderColor: 'rgba(0,0,0,.1)',
+  appContentBg: '#ffffff',
+  appBorderColor: '#585C6D',
   appBorderRadius: 4,
 
   // Typography
@@ -22,37 +22,17 @@ const theme = create({
   // Text colors
   textColor: '#10162F',
   textInverseColor: '#ffffff',
-  textMutedColor: '#FF5F00',
 
-
+  // Form colors
+  inputBg: '#ffffff',
+  inputBorder: '#10162F',
+  inputTextColor: '#10162F',
+  inputBorderRadius: 2,
 
   // Toolbar default and active colors
-  // barTextColor: '#DD4411',
-  // barSelectedColor: '#45bc5a',
-  // barBg: '#ff515d',
-
-  // // Form colors
-  // inputBg: '#ff4000',
-  // inputBorder: 'rgba(0,0,0,.3)',
-  // inputTextColor: '#ff48ff',
-  // inputBorderRadius: 4,
-
-    // Toolbar default and active colors
-    barTextColor: '#9E9E9E',
-    barSelectedColor: '#585C6D',
-    barBg: '#ffffff',
-  
-    // Form colors
-    inputBg: '#ffffff',
-    inputBorder: '#10162F',
-    inputTextColor: '#10162F',
-    inputBorderRadius: 2,
-
-  // Brand assets
-  // brandTitle: 'Newline Storybook',
-  // brandUrl: 'https://newline.co',
-  // brandImage:
-  //   'https://d8dgeb1f3fxgw.cloudfront.net/static/img/logo/newline/newline-logo-longwise-solo-lightbg-gray.svg',
+  barTextColor: '#9E9E9E',
+  barSelectedColor: '#585C6D',
+  barBg: '#ffffff',
 })
 
 addons.setConfig({
