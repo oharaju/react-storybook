681ed2f91684019c7399b3cdba4972b3e98de1e8
Júlia Ohara <juliaohara.br@gmail.com>
2023-08-09 22:32:42 -0300

    feat: creation of accordion component

---
+++ src/components/Atoms/Accordion/index.jsx
@@ -0,0 +1,13 @@
+import { Container }  from './styles.js';
+
+export default function Accordion ()  {
+  return(
+    <Container>
+      <h2>Hypertext Accordion</h2>
+      <div>
+        <p>Details</p>
+        <div>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex.</div>
+      </div>
+    </Container>
+  )
+}
