fe543cd8c25c22418834251c3ac646a962e2fad0
Júlia Ohara <juliaohara.br@gmail.com>
2023-08-10 21:39:24 -0300

    feat: fix accordion component style

--- src/components/Atoms/Accordion/styles.js
+++ src/components/Atoms/Accordion/styles.js
@@ -3,3 +3,19 @@ import { styled } from "styled-components";
 export const Container = styled.div`
   padding: 10px;
 `
+
+export const Summary = styled.summary`
+  font-size: 12px;
+`
+
+export const Details = styled.details`
+  width: 100%;
+  /* padding: 0.5rem 0; */
+  border-top: 1px solid black;
+  position: relative;
+  cursor: pointer;
+  font-size: 1.25rem;
+  font-weight: 300;
+  list-style: none;
+
+`
