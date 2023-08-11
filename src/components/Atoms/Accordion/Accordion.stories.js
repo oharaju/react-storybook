190ffa665de7de0ab8ad7804ffc87eeb805fb910
Júlia Ohara <juliaohara.br@gmail.com>
2023-08-10 21:40:37 -0300

    feat: create accordion component stories file

---
+++ src/components/Atoms/Accordion/Accordion.stories.js
@@ -0,0 +1,18 @@
+import Accordion from '.';
+
+export default {
+  title: 'Design System/Atoms/Accordion',
+  component: Accordion,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+}
+
+export const Details = {
+  args: {
+    subtitle: 'Details',
+    resume: 'Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex.'
+  },
+};
+
