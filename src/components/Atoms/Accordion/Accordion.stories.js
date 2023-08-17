c574dca0b7728798d1f396d60c21ff24e0013500
Júlia Ohara <juliaohara.br@gmail.com>
2023-08-16 23:00:35 -0300

    feat: add items to accordion component

--- src/components/Atoms/Accordion/Accordion.stories.js
+++ src/components/Atoms/Accordion/Accordion.stories.js
@@ -9,10 +9,22 @@ export default {
   tags: ['autodocs'],
 }
 
-export const Details = {
+export const Features = {
   args: {
-    subtitle: 'Details',
-    resume: 'Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex.'
+    title: 'Hypertext Accordion',
+    list: [
+      {
+        subtitle: 'Details',
+        resume: 'Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex.'
+      },
+      {
+        subtitle: 'Features',
+        resume: 'Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex.'
+      },
+      {
+        subtitle: 'Information',
+        resume: 'Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex.'
+      }
+    ],
   },
 };
-
