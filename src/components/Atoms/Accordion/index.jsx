1b9a2fad17ce42e1703acfc4172934bafd846828
Júlia Ohara <juliaohara.br@gmail.com>
2023-08-16 22:59:29 -0300

    feat: add map to accordion component

--- src/components/Atoms/Accordion/index.jsx
+++ src/components/Atoms/Accordion/index.jsx
@@ -1,19 +1,24 @@
-import { Container, Details, Summary }  from './styles.js';
+import { Container, Details, Summary, Resume }  from './styles.js';
 import { PropTypes } from 'prop-types';
 
-export default function Accordion ( { subtitle, resume } )  {
+export default function Accordion ( { title, list } )  {
   return(
     <Container>
-      <h2>Hypertext Accordion</h2>
-      <Details>
-        <Summary>{subtitle}</Summary>
-        <p>{resume}</p>
-      </Details>
+      <h2>{title}</h2>
+      {list.map((item) => 
+        <Details key={item.subtitle}>
+          <Summary>{item.subtitle}</Summary>
+          <Resume>{item.resume}</Resume>
+        </Details>
+      )}
     </Container>
   )
 }
 
 Accordion.propTypes = {
-  subtitle: PropTypes.oneOf(['Details', 'Features', 'Information', 'Specifications']),
-  resume: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  list : PropTypes.arrayOf(PropTypes.shape({
+    subtitle: PropTypes.string.isRequired,
+    resume: PropTypes.string.isRequired
+  }))
 }
