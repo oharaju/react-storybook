04aad1b8b8cdfb631382ffa087cc46e039dc23ed
Júlia Ohara <juliaohara.br@gmail.com>
2023-08-21 22:02:20 -0300

    feat: adjust accordion component stylesheet

--- src/components/Atoms/Accordion/styles.js
+++ src/components/Atoms/Accordion/styles.js
@@ -1,21 +1,32 @@
 import { styled } from "styled-components";
 
 export const Container = styled.div`
+  font-family: "Gill Sans Extrabold", sans-serif;
   padding: 10px;
+  width: 100%;
+  min-width: 320px;
+  max-width: 320px;
 `
 
 export const Summary = styled.summary`
-  font-size: 12px;
+  font-size: 20px;
+  list-style: none;
 `
 
 export const Details = styled.details`
   width: 100%;
-  /* padding: 0.5rem 0; */
+  padding: 0.5rem 0;
   border-top: 1px solid black;
   position: relative;
-  cursor: pointer;
   font-size: 1.25rem;
   font-weight: 300;
   list-style: none;
+  cursor: pointer;
+`
 
+export const Resume = styled.p`
+  font-size: 0.95rem;
+  margin: 0 0 1rem;
+  padding-top: 1rem;
+  line-height: 1.4;
 `
